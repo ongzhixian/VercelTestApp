8e430c1bdbd1559f80036dc696538fa8e9f40ff8
zhixian <zhixian@hotmail.com>
2023-09-12 19:42:53 +0800

    asd

--- vercel-test-app/src/app/page.js
+++ vercel-test-app/src/app/page.js
@@ -1,8 +1,10 @@
 export default function Page() {
     return <>
         <main class="my-0 mx-auto max-w-3xl text-center">
-
+            
         <h2 class="p-6 text-4xl">A Basic Tailwind CSS Example</h2>
+        
+        <p class="px-10 pb-10 text-left">Tailwind CSS works by scanning all of your HTML files, JavaScript components, and any other templates for class names, generating the corresponding styles and then writing them to a static CSS file. It's fast, flexible, and reliable — with zero-runtime.</p>
         
         </main>
     </>
